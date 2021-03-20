87bd06dd4c986c95b9141a2a38bd46f113ce53ef
Vitalik_Zubik <leo_thebest@mail.ru>
2021-03-21 00:21:55 +0300

    homework 2, task 1

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
+const btnRef1 = document.querySelector('.homework_1'),
+      btnRef2 = document.querySelector('.homework_2'),
+      wraperHomework1 = document.querySelector('.wraper_homework_1'),
+      wraperHomework2 = document.querySelector('.wraper_homework_2');
+
+btnRef1.addEventListener('click', () => {
+    if(wraperHomework1.classList.contains('not_active_homework')) {
+        wraperHomework1.classList.remove('not_active_homework');
+        wraperHomework2.classList.add('not_active_homework');
+        btnRef1.classList.add('active_ref');
+        btnRef2.classList.remove('active_ref');
+    } else {
+        return
+    }
+});
+
+btnRef2.addEventListener('click', () => {
+    if(wraperHomework2.classList.contains('not_active_homework')) {
+        wraperHomework2.classList.remove('not_active_homework');
+        wraperHomework1.classList.add('not_active_homework');
+        btnRef2.classList.add('active_ref');
+        btnRef1.classList.remove('active_ref'); 
+    } else {
+        return
+    }
+});
+                //  HOMEWORK - 1
 // 1. Даны кнопки. Привяжите к каждой кнопке событие по клику, 
 // которое будет считать количество нажатий по кнопке и выводить его в текст кнопки.
 // Количество нажатий для каждой кнопки должно хранится в замыкании
-
 const btnOne = document.querySelector('.btn_one'),
-      btnTwo = document.querySelector('.btn_two'),
-      p1 = document.getElementById('1'),
-      p2 = document.getElementById('2'),
-      p3 = document.getElementById('3'),
-      p4 = document.getElementById('4');
-
+      btnTwo = document.querySelector('.btn_two');
 
 const onBtn = (elem) => {
     let count = 0;
@@ -30,6 +51,11 @@ btnTwo.addEventListener('click', onBtnTwo);
 // 2. Дан массив цветов. Даны абзацы.
 //  По первому нажатию на абзац он должен покраситься в первый цвет из массива, 
 //  по второму нажатию - во второй и так далее. Все абзацы работают независимо.
+const p1 = document.getElementById('1'),
+      p2 = document.getElementById('2'),
+      p3 = document.getElementById('3'),
+      p4 = document.getElementById('4');
+
 const arrayColor = ['red', 'blue', 'yellow', 'orange', 'green', 'purple', 'pink', 'white'];
 
 const setColor = (elem) => {
@@ -101,7 +127,7 @@ const btnRandom = document.querySelector('.btn_random').addEventListener('click'
 // 4.  Дан массив с элементами [2, 3, 4, 5]. С помощью цикла for найдите произведение элементов этого массива.
 // на доп балл: использовать метод массива reduce.
 const btnResultMultiplication = document.querySelector('.multiply'),
-      spanResultMultiplication = document.getElementById('6') 
+      spanResultMultiplication = document.getElementById('6'); 
 
 const arrayNum = [2, 3, 4, 5];
 
@@ -115,7 +141,7 @@ const onMultiplication = () => {
         return sum * curent; 
     },1)
 
-    spanResultMultiplication.innerHTML = ` ${result}`
+    spanResultMultiplication.innerHTML = ` ${result}`;
 }
 
 btnResultMultiplication.addEventListener('click', onMultiplication);
@@ -166,7 +192,7 @@ const arrNumbers = [4, 2, 5, 19, 13, 0, 10];
 
 const onSquareRoot = () => {
     let sum = 0;
-    for (let i=0; i<arrNumbers.length;i++) {
+    for (let i=0; i<arrNumbers.length; i++) {
         sum = sum + Math.pow(arrNumbers[i], 3);
     }
 
@@ -213,3 +239,81 @@ const onClickDivision = () => {
 inputA.addEventListener('input', () => {a = inputA.value});
 inputB.addEventListener('input', () => {b = inputB.value});
 btn.addEventListener('click', onClickDivision);
+
+            // HOMEWORK - 2
+
+//1. Сравнить массивы
+// var a=[2,3,5];
+// var b=[2,3,5];
+// var c=[2,2,3,3,5,6]
+// var d=[1,2,3]
+const inputArr1 = document.getElementById('13'),
+      inputArr2 = document.getElementById('14'),
+      btnResultCompare = document.getElementById('15'),
+      pCompareResult = document.querySelector('.compare_result');
+
+let resultCompareArrays = '';
+let arrA = [],
+    arrB = [];
+
+const resultCompare = () => {
+
+    const compareArrays = (arr1, arr2) => {
+        if(arr1.length !== arr2.length) {
+            return resultCompareArrays = false;
+        }
+
+        for (let i = 0; i < arr1.length; i++) {
+            if (!arr2.includes(arr1[i])) {
+                return resultCompareArrays = false;
+            }
+        }
+
+        return resultCompareArrays = true;
+    }
+
+    compareArrays(arrA,arrB);
+    
+    if(resultCompareArrays) {
+        pCompareResult.innerHTML = 'Массивы 1 и 2 равны.'
+    } else {
+        pCompareResult.innerHTML = 'Массивы 1 и 2 не равны.'
+    }
+}
+inputArr1.addEventListener('input', () => {arrA = inputArr1.value.toLowerCase().split(', ')});
+inputArr2.addEventListener('input', () => {arrB = inputArr2.value.toLowerCase().split(', ')});
+
+btnResultCompare.addEventListener('click', resultCompare)
+
+
+// 2. Вывести уникальные эелементы (цикл в цикле) и (используя {} (Map или HashMap)) 
+// использовать for, forEach, reduce 
+// const myArray1 = [55,77,66];
+// const myArray2 = [55,11,15,77,66,99,100,110];
+
+
+
+// 3. Дана строка. Сделайте заглавным первый символ этой строки не используя цикл. 
+// Найдите два решения.
+
+// 4. Дана строка, например, '123456'. Переверните эту строку (сделайте из нее '654321') 
+// не используя цикл.
+
+// 5. Найдите квадратный корень из 587. Округлите результат в большую и меньшую стороны, 
+// запишите результаты округления в объект с ключами 'floor' и 'ceil'.
+
+// 6. Даны числа 4, -2, 5, 19, -130, 0, 10. Найдите минимальное и максимальное число 
+// (Math.min, Math.max).
+
+// 7. Заполните массив 10-ю случайными целыми числами. Посчитайте их сумму с помощью reduce, 
+// создайте новый массив в котором будут квадраты этих чисел, используя map, 
+// оставьте отфильтруйте четные, отсортируйте по убыванию их квадратных корней 
+// (целой части корня).
+
+// 8. Дан объект {js:['jQuery', 'Angular'], php: 'hello', css: 'world'}. 
+// Выведите с его помощью слово 'jQuery'.
+
+// 9. Создайте двухмерный массив. Первые два ключа - это 'ru' и 'en'. 
+// Пусть первый ключ содержит элемент, являющийся массивом названий дней недели по-русски, 
+// а второй - по-английски. Выведите с помощью этого массива понедельник по-русски и 
+// среду по английски (пусть понедельник - это нулевой день).
