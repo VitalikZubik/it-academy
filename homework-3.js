6eae946ff8d1c49a97e60bcf7bc43893a8146e3d
Vitalik_Zubik <leo_thebest@mail.ru>
2021-04-06 15:40:35 +0300

    homework 3, task 2

--- homework-3.js
+++ homework-3.js
@@ -17,3 +17,34 @@ const palindrome = () => {
 
 inputPalindrome.addEventListener('input', () => {strPalindrome = inputPalindrome.value});
 btnPalindrome.addEventListener('click', palindrome);
+
+// 2.Требуется написать функцию, выводящую в консоль числа от 1 до n, 
+//где n — это целое число, которая функция принимает в качестве параметра, с такими условиями:
+//вывод fizz вместо чисел, кратных 3;
+//вывод buzz вместо чисел, кратных 5;
+//вывод fizzbuzz вместо чисел, кратных как 3, так и 5.
+
+const inputFizzBuzz = document.getElementById('48'),
+      pFizzBuzz = document.getElementById('49'),
+      btnFizzBuzz = document.getElementById('50');
+
+let numFizzBuzz = 0;
+
+const fizzBuzz = () => {
+    for (let i = 1; i <= numFizzBuzz; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            pFizzBuzz.innerHTML += ' fizzbuzz,';
+        } else if (i % 3 === 0) {
+            pFizzBuzz.innerHTML += ' fizz,';
+        } else if (i % 5 === 0) {
+            pFizzBuzz.innerHTML += ' buzz,';
+        } else {
+            pFizzBuzz.innerHTML += ` ${i},`;
+        }
+    }
+};
+
+inputFizzBuzz.addEventListener('input', () => { numFizzBuzz = inputFizzBuzz.value});
+btnFizzBuzz.addEventListener('click', fizzBuzz);
+
+// 3.
