ce4c1f0b5595c11f1192ab51fe8decba0c9b6707
Vitalik_Zubik <leo_thebest@mail.ru>
2021-04-21 20:41:13 +0300

    homework 4, task 2

--- homework-4.js
+++ homework-4.js
@@ -20,3 +20,50 @@ const sum = (a) => {
 const resultSum = sum(1)(2)(3)(4)(5);
 
 divSum.append(resultSum);
+
+// 2 Написать свой bind()
+
+const obj = {
+    name: 'Vitalik'
+}
+
+const test = function (phone, email) {
+    console.log(`Name: ${this.name}, tel: ${phone}, email: ${email}`);
+}
+
+// const bind = (fn, context, ...rest) => {
+//     return function (...args) {
+//         return fn.call(context, ...rest, ...args);
+//     }
+// }
+
+const bind = function (fn, context, ...rest) {
+    return function (...args) {
+        const uniqueId = Date.now().toString();
+
+        context[uniqueId] = fn;
+
+        const result = context[uniqueId](...rest, ...args);
+
+        delete context[uniqueId];
+
+        return result;
+    }
+    
+
+}
+
+bind(test, obj)(123123, 'g@mail.ru');
+bind(test, obj, 123123)('g@mail.ru');
+bind(test, obj, 123123, 'g@mail.ru')();
+
+// 3 есть объект:
+// {
+//     name,
+//     surname,
+//     phone,
+// }
+//     сделать массив из 10 подобных объектов, потом добавить к каждому элементу массива id и дату.
+//     Сделать таблицу, в которой будут колонки N, Имя, Фамилия, Телефон, Дата.
+//     Таблицу динамически создать
+
