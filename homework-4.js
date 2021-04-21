4dfef9673fbb68e03aa11a6007d5b5b5400a0ff9
Vitalik_Zubik <leo_thebest@mail.ru>
2021-04-21 23:14:12 +0300

    homework 4, task 3

--- homework-4.js
+++ homework-4.js
@@ -67,3 +67,56 @@ bind(test, obj, 123123, 'g@mail.ru')();
 //     Сделать таблицу, в которой будут колонки N, Имя, Фамилия, Телефон, Дата.
 //     Таблицу динамически создать
 
+const btnResultTable = document.querySelector('.btn_table'),
+      divTable = document.querySelector('.table');
+
+const arrayList = [
+    {name: 'Alex', surname: 'Ivanov', phone: 375291234567},
+    {name: 'Oleg', surname: 'Ivanov', phone: 375291235467},
+    {name: 'Yana', surname: 'Ivanova', phone: 375292134567},
+    {name: 'Artem', surname: 'Ivanov', phone: 375291324567},
+    {name: 'Ivan', surname: 'Ivanov', phone: 375291234576},
+    {name: 'Vitalik', surname: 'Ivanov', phone: 375291234657},
+    {name: 'Olya', surname: 'Ivanova', phone: 375291234765},
+    {name: 'Anna', surname: 'Ivanova', phone: 375291235467},
+    {name: 'Alisa', surname: 'Ivanova', phone: 375292234567},
+    {name: 'Petr', surname: 'Ivanov', phone: 375293234567}
+];
+
+arrayList.forEach((elem, index) => {
+    elem.id = index + 1;
+    elem.date = Date();
+    return elem;
+});
+
+const table = document.createElement('table');
+
+table.insertAdjacentHTML('beforeend', `
+                            <thead>
+                                <tr>
+                                    <td>N</td>
+                                    <td>Имя</td>
+                                    <td>Фамилия</td>
+                                    <td>Телефон</td>
+                                    <td>Дата</td>
+                                </tr>
+                            </thead>
+                            `);
+
+arrayList.map(({name, surname, phone, id, date}) => {
+    table.insertAdjacentHTML('beforeend', `
+                            <tbody>
+                                <tr>
+                                    <td>${id}</td>
+                                    <td>${name}</td>
+                                    <td>${surname}</td>
+                                    <td>${phone}</td>
+                                    <td>${date}</td>
+                                </tr>
+                            </tbody>
+                            `)
+});
+
+btnResultTable.addEventListener('click', () => {
+    divTable.append(table);
+});
